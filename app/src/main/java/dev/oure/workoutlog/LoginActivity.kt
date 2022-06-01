95e5c10a548869700e05090d41359c054c3e7bd8
christineoure <ourechristine@gmail.com>
2022-06-01 21:08:57 +0300

    add workoutlog

--- app/src/main/java/dev/oure/workoutlog/LoginActivity.kt
+++ app/src/main/java/dev/oure/workoutlog/LoginActivity.kt
@@ -1,11 +1,39 @@
 package dev.oure.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var tvSignup: TextView
+    lateinit var etEmail: TextInputEditText
+    lateinit var etPassword: TextInputEditText
+    lateinit var btnLogin: Button
+    lateinit var tilEmail: TextInputLayout
+    lateinit var tilPassword: TextInputLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        tvSignup=findViewById(R.id.tvSignup)
+        etEmail=findViewById(R.id.etEmail)
+        etPassword=findViewById(R.id.etPassword)
+        btnLogin=findViewById(R.id.btnLogin)
+        tilEmail=findViewById(R.id.tilEmail)
+        tilPassword=findViewById(R.id.tilPassword)
+        tvSignup.setOnClickListener {
+            var intent =Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        btnLogin.setOnClickListener {
+                 validate()
+        }
+    }
+    fun validate(){
+        var email = etEmail.text.toString()
+        var password = etPassword.text.toString()
     }
 }
