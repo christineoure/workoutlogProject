6c0b91f865c660bd11747967bfb8b39b04401def
christineoure <ourechristine@gmail.com>
2022-06-14 20:05:17 +0300

    added fragment activities

--- app/src/main/java/dev/oure/workoutlog/HomeActivity.kt
+++ app/src/main/java/dev/oure/workoutlog/HomeActivity.kt
@@ -2,10 +2,40 @@ package dev.oure.workoutlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var bottomnavigation: BottomNavigationView
+    lateinit var fcvHome: FragmentContainerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        castViews()
+        setupBottomNav()
+    }
+    fun castViews(){
+        bottomnavigation=findViewById(R.id.bottom_navigation)
+        fcvHome=findViewById(R.id.fcvHome)
+    }
+    fun setupBottomNav(){
+        bottomnavigation.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.plan->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, PlanFragment()).commit()
+                    true
+                }
+                R.id.track->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, TrackFragment()).commit()
+                    true
+                }
+                R.id.profile->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, ProfileFragment()).commit()
+                    true
+                }
+                else-> false
+            }
+        }
     }
 }
